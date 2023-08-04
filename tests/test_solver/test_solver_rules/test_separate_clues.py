cdcbbc8161c966348df98585f103d1716d53d53d
Sam Pfeiffer <sampfeiffer54@gmail.com>
2023-08-04 16:15:18 -0400

    Add unit tests for separate clue solver rule

---
+++ tests/test_solver/test_solver_rules/test_separate_clues.py
@@ -0,0 +1,76 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from nurikabe.board import Board
+from nurikabe.solver.solver_rules.separate_clues import SeparateClues
+from tests.build_board import build_board
+
+
+class TestBoardStateChecker(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.screen = MagicMock(name='Screen')
+
+    def create_board(self, board_details: list[str]) -> Board:
+        return build_board(self.screen, board_details)
+
+    def test_no_cell_changes(self) -> None:
+        """Ensure that when there are no clues to separate, the board state is not impacted by this solver rule."""
+        board_details = [
+            '1,_,_,_',
+            '_,_,_,_',
+            '_,3,_,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = SeparateClues(board).apply_rule()
+        self.assertFalse(cell_changes.has_any_changes())
+        self.assertEqual(board.as_simple_string_list(), board_details)
+
+    def test_separate_clues(self) -> None:
+        """Cells that are adjacent to two clues should be marked as walls."""
+        board_details = [
+            '1,_,3,_',
+            '_,_,_,2',
+            '_,_,_,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = SeparateClues(board).apply_rule()
+        self.assertTrue(cell_changes.has_any_changes())
+        expected_board_state = [
+            '1,X,3,X',
+            '_,_,X,2',
+            '_,_,_,_'
+        ]
+        self.assertEqual(board.as_simple_string_list(), expected_board_state)
+
+    def test_separate_three_clues(self) -> None:
+        """Cells that are adjacent to more than two clues should also be marked as walls."""
+        board_details = [
+            '1,_,3,_',
+            '_,2,_,_',
+            '_,_,_,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = SeparateClues(board).apply_rule()
+        self.assertTrue(cell_changes.has_any_changes())
+        expected_board_state = [
+            '1,X,3,_',
+            'X,2,X,_',
+            '_,_,_,_'
+        ]
+        self.assertEqual(board.as_simple_string_list(), expected_board_state)
+
+    def test_clues_already_separated(self) -> None:
+        """
+        If cells that are adjacent to more than one clue are already marked as walls, then this solver rule has no
+        impact.
+        """
+        board_details = [
+            '1,X,3,_',
+            'X,2,X,_',
+            '_,X,1,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = SeparateClues(board).apply_rule()
+        self.assertFalse(cell_changes.has_any_changes())
+        self.assertEqual(board.as_simple_string_list(), board_details)
