c82f4cdc2458aa4172fb1df777aa95027ebb5eb2
Sam Pfeiffer <sampfeiffer54@gmail.com>
2023-08-05 10:35:40 -0400

    Add unit tests for the naively unreachable from clue cell solver rule

---
+++ tests/test_solver/test_solver_rules/test_naively_unreachable_from_clue_cell.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from nurikabe.board import Board
+from nurikabe.solver.solver_rules.naively_unreachable_from_clue_cell import NaivelyUnreachableFromClueCell
+from tests.build_board import build_board
+
+
+class TestNaivelyUnreachableFromClueCell(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.screen = MagicMock(name='Screen')
+
+    def create_board(self, board_details: list[str]) -> Board:
+        return build_board(self.screen, board_details)
+
+    def test_all_cells_manhattan_reachable(self) -> None:
+        """A cells are Manhattan reachable from a clue cell, so the board is not impacted by this solver rule."""
+        board_details = [
+            '_,_,_,_,_,_',
+            '_,4,_,_,3,_',
+            '_,_,_,_,_,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = NaivelyUnreachableFromClueCell(board).apply_rule()
+        self.assertFalse(cell_changes.has_any_changes())
+        self.assertEqual(board.as_simple_string_list(), board_details)
+
+    def test_manhattan_reachable_through_walls(self) -> None:
+        """
+        This solver rule is a naive (and therefore computationally cheap) reachability check. It does not check if there
+        are walls in the way when determining if a cell is reachable by a clue cell.
+        """
+        board_details = [
+            '_,_,X,_',
+            '_,4,X,_',
+            '_,_,X,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = NaivelyUnreachableFromClueCell(board).apply_rule()
+        self.assertFalse(cell_changes.has_any_changes())
+        self.assertEqual(board.as_simple_string_list(), board_details)
+
+    def test_unreachable_cells_from_clue(self) -> None:
+        """Empty cells that are Manhattan unreachable from a clue cell are marked as walls."""
+        board_details = [
+            '_,_,_,_',
+            '_,_,_,X',
+            '_,3,X,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = NaivelyUnreachableFromClueCell(board).apply_rule()
+        self.assertTrue(cell_changes.has_any_changes())
+        expected_board_state = [
+            'X,_,X,X',
+            '_,_,_,X',
+            '_,3,X,_'
+        ]
+        self.assertEqual(board.as_simple_string_list(), expected_board_state)
+
+    def test_non_wall_cells_not_impacted(self) -> None:
+        """
+        This solver rule only impacts empty cells. Cells that are (incorrectly) marked as non-walls are not impacted.
+        """
+        board_details = [
+            '_,_,O,_',
+            '_,_,_,X',
+            '_,3,X,_'
+        ]
+        board = self.create_board(board_details)
+        cell_changes = NaivelyUnreachableFromClueCell(board).apply_rule()
+        self.assertTrue(cell_changes.has_any_changes())
+        expected_board_state = [
+            'X,_,O,X',
+            '_,_,_,X',
+            '_,3,X,_'
+        ]
+        self.assertEqual(board.as_simple_string_list(), expected_board_state)
