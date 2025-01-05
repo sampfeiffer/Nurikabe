8519d130b155719e76092bf32322c1b4f9bfc4f1
Sam Pfeiffer <sampfeiffer54@gmail.com>
2025-01-05 16:04:30 -0500

    Fix ruff issue

--- nurikabe/color.py
+++ nurikabe/color.py
@@ -1,13 +1,12 @@
 from enum import Enum
-from typing import ClassVar
 
 
 class Color(Enum):
-    BLACK = 3 * [0]
-    OFF_WHITE = 3 * [240]
-    GRAY = 3 * [160]
-    LIGHT_SLATE_GRAY = [178, 204, 229]
-    MEDIUM_SLATE_GRAY = [119, 136, 153]
-    GREEN = [0, 128, 0]
-    RED = [255, 0, 0]
-    YELLOW = [255, 255, 0]
+    BLACK = (0, 0, 0)
+    OFF_WHITE = (240, 240, 240)
+    GRAY = (160, 160, 160)
+    LIGHT_SLATE_GRAY = (178, 204, 229)
+    MEDIUM_SLATE_GRAY = (119, 136, 153)
+    GREEN = (0, 128, 0)
+    RED = (255, 0, 0)
+    YELLOW = (255, 255, 0)
